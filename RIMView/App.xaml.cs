0b11bd6eba1ba562ba510a46457def1383dc1060
William <william@wtdawson.info>
2024-11-08 20:58:25 +0000

    Add project files.

---
+++ RIMView/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace RIMView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if(e.Args.Length != 1)
+            {
+                MessageBox.Show("Failed to load RIM file: No argument provided", "RIMView");
+                Environment.Exit(-1);
+            }
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            mainWindow.SetLoading(true);
+
+            if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RIMView", "Temp")))
+            {
+                Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RIMView", "Temp"));
+            }
+
+            string location = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "RIMView", "Temp", new Guid().ToString() + ".bmp");
+
+            try
+            {
+                Process process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "RIMTils", "RIMTils.exe"),
+                        Arguments = $"convert -m ToBMP -f \"{e.Args[0]}\" -t \"{location}\"",
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                process.WaitForExit();
+
+                mainWindow.SetLoading(false);
+                mainWindow.SetImage(location);
+            } catch (Exception ex)
+            {
+                mainWindow.Hide();
+                MessageBox.Show($"Failed to load RIM file: {ex.Message}", "RIMView");
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
---
+++ RIMView/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RIMView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetLoading(bool loading)
+        {
+            Message.Visibility = Visibility.Visible;
+            Message.Text = "Loading...";
+        }
+
+        public void SetImage(string imagePath)
+        {
+            Img.Source = new BitmapImage(new Uri(imagePath));
+        }
+    }
+}
